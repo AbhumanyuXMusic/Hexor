59f67a785696b059c4d32dd95ecd266781b87f44
AbhimanyuRana <91766052+AbhumanyuXMusic@users.noreply.github.com>
2021-12-14 08:19:27 +0530

    Update song.py

--- handlers/song.py
+++ handlers/song.py
@@ -22,7 +22,7 @@ def song(client, message):
     for i in message.command[1:]:
         query += ' ' + str(i)
     print(query)
-    m = message.reply('𝐅𝐢𝐧𝐝𝐢𝐧𝐠 💫 𝐓𝐡𝐞 𝐒𝐨𝐧𝐠 🎧 𝐅𝐫𝐨𝐦 𝐂𝐚𝐧𝐝𝐲 🍬 𝐌𝐮𝐬𝐢𝐜𝐗  ❤️ 𝐒𝐞𝐫𝐯𝐞𝐫 🌎...')
+    m = message.reply('𝐅𝐢𝐧𝐝𝐢𝐧𝐠 💫 𝐓𝐡𝐞 𝐒𝐨𝐧𝐠 🎧 𝐅𝐫𝐨𝐦 ✰ 𝕋𝕒𝕓𝕒𝕙𝕚 ☠️ 𝕊𝕖𝕣𝕧𝕖𝕣 ✰ 🌎...')
     ydl_opts = {"format": "bestaudio[ext=m4a]"}
     try:
         results = YoutubeSearch(query, max_results=1).to_dict()
@@ -45,13 +45,13 @@ def song(client, message):
         )
         print(str(e))
         return
-    m.edit("𝐃𝐨𝐰𝐧𝐥𝐨𝐚𝐝 ✨ 𝐒𝐨𝐧𝐠 🎸 𝐅𝐫𝐨𝐦 𝐂𝐚𝐧𝐝𝐲 🍬 𝐌𝐮𝐬𝐢𝐜 🥀 𝐒𝐞𝐫𝐯𝐞𝐫 🌎...")
+    m.edit("𝐃𝐨𝐰𝐧𝐥𝐨𝐚𝐝 ✨ 𝐒𝐨𝐧𝐠 🎸 𝐅𝐫𝐨𝐦 ✰ 𝕋𝕒𝕓𝕒𝕙𝕚 ☠️ 𝕊𝕖𝕣𝕧𝕖𝕣 ✰ 🌎...")
     try:
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = '**🎵 𝐔𝐩𝐥𝐨𝐚𝐝𝐞𝐝 𝐁𝐲 :- ✨ @Sanki_Owner ❤️**'
+        rep = '**🎵 𝐔𝐩𝐥𝐨𝐚𝐝𝐞𝐝 𝐁𝐲 :- ✨ @GoluBhaiya_0 ❤️**'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
